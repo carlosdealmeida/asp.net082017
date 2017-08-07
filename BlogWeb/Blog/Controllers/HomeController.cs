f85efd580a64e10c2ff9f0bea65c6680385b6a7d
Carlos Junior <carlos.dealmeiida@gmail.com>
2017-08-07 20:31:34 -0300

    Iniciando o projeto BlogWeb

---
+++ BlogWeb/Blog/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Blog.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var lista = new List<Post>();
+            lista.Add(new Post { Titulo = "Harry Potter", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" });
+            lista.Add(new Post { Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" });
+            lista.Add(new Post { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" });
+            lista.Add(new Post { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" });
+
+            return View(lista);
+        }
+    }
+}
